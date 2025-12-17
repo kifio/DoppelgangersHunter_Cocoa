8724f00a5d8fd44b54047f945bfacd333e00079e
imurashov-belkacar <imurashov@belkacar.org>
2025-12-17 23:31:03 +0300

    Initial Commit

---
+++ DoppegannersHunter-Cocoa/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DoppegannersHunter-Cocoa
+//
+//  Created by imurashov on 17.12.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
